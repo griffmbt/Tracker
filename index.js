ff87f076d90586b86238f15afa7870f9cdf24574
Renat Hi <renathi@MacBook-Pro-Renat.local>
2021-01-23 14:08:15 +0400

    added a color change to the button 'sortButton' and fix bugs

--- index.js
+++ index.js
@@ -1,20 +1,91 @@
 function addTrack() {
+
+    function showSortingIncreasing() {
+        const textInputs2 = document.querySelectorAll('.text-input');
+        const trackers = document.querySelectorAll('.tracker');
+        let sorted = Array.from(textInputs2).sort((a, b) => {
+            let x = a.value;
+            let y = b.value;
+
+            if(x > y) {
+                return 1;
+            } else if (x < y) {
+                return -1;
+            } else {
+                return 0;
+            }
+            
+        });
+        
+        for (let i = 0; i < trackers.length; i++) {
+           trackers[i].insertBefore(sorted[i], trackers[i].children[1]);
+        };
+        
+        return sorted;
+    }
+
+    function showSortingDecreasing() {
+        const textInputs2 = document.querySelectorAll('.text-input');
+        const trackers = document.querySelectorAll('.tracker');
+        let sorted = Array.from(textInputs2).sort((a, b) => {
+            let x = a.value;
+            let y = b.value;
+
+            if(x > y) {
+                return -1;
+            } else if (x < y) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        
+        for (let i = 0; i < trackers.length; i++) {
+           trackers[i].insertBefore(sorted[i], trackers[i].children[1]);
+        };
+        
+        return sorted;
+    };
+
+
     track.append(tracker.cloneNode(true));
     const trackers = document.querySelectorAll('.tracker');
+    const buttonDelete = document.querySelectorAll('.group56');
+    const group36 = document.querySelector('.group36');
+    const textInputs = document.querySelectorAll('.text-input');
+    const sortButton = document.querySelector('.group34');
+    let sortButtonTrecker;
+
+    track.addEventListener('dragenter', (evt) => {
+        evt.target.parentElement.style['border-bottom'] = 'solid 3px blueviolet';
+        evt.preventDefault();
+    })
+    
+    track.addEventListener('dragover', (evt) => {
+        evt.preventDefault();
+    })
+    
+    track.addEventListener('dragleave', (evt) => {
+        evt.target.parentElement.style['border-bottom'] = '';
+        evt.target.style.backgroundColor = 'unset';
+    })
+    
+    track.addEventListener('drop', (evt) => {
+        evt.target.parentElement.style['border-bottom'] = '';
+        track.insertBefore(dragElement, evt.target.parentElement);
+    })
+
     trackers.forEach((item) => {
         item.addEventListener('dragstart', (evt) => {
-            evt.target.style.backgroundColor = '#FFDC40';
-            // textInputs.style.backgroundColor = '#FFDC40'
-            dragElement = evt.target;
-        })
+           item.style.backgroundColor = '#FFDC40';
+            dragElement = item;
+        });
     
         item.addEventListener('dragend', (evt) => {
-            evt.target.style.backgroundColor = 'unset';
+           item.style.backgroundColor = 'unset';
             dragElement = null;
-        })
+        });
     });
-
-    const buttonDelete = document.querySelectorAll('.group56')
 
     buttonDelete.forEach((item) => {
         item.addEventListener('mouseover', (event) => {
@@ -23,85 +94,53 @@ function addTrack() {
         item.addEventListener('mouseout', (event) => {
             event.target.src = 'img/Group56.svg';
         });
-        //const parentElement = document.querySelectorAll('.tracker')
         item.addEventListener('click', () => {
             item.parentElement.remove();
         });
     });
     
-
-    function showSortingIncreasing(textInputs) {
-        let sorted = Array.from(textInputs).sort((a, b) => {
-            let x = a.value;
-            let y = b.value;
-
-            if(x > y) {
-                return 1;
-            } else if (x < y) {
-                return -1;
-            }
-            
-        });
+    tracker.addEventListener('mousedown', (evt) => {
         
-        for (let i = 0; i < trackers.length; i++) {
-           trackers[i].insertBefore(sorted[i], trackers[i].children[1])
+        if(evt.target === group36) {
+            evt.target.draggable = true;
+        } else {
+            evt.target.draggable = false;
         };
-        
-        return sorted;
+    });
 
-    }
-
-    function showSortingDecreasing(textInputs) {
-        let sorted = Array.from(textInputs).sort((a, b) => {
-            let x = a.value;
-            let y = b.value;
-
-            if(x > y) {
-                return -1;
-            } else if (x < y) {
-                return 1;
-            }
-            
-        });
-        
-        for (let i = 0; i < trackers.length; i++) {
-           trackers[i].insertBefore(sorted[i], trackers[i].children[1])
-        }
-        
-        return sorted;
-    };
-
-    const textInputs = document.querySelectorAll('.text-input');
-   
-    let sortButtonTrecker;
     sortButtonUP.addEventListener('click', () => {
-    const sortButton = document.querySelector('.group34')
+        const sortButton = document.querySelector('.group34');
     
         if (sortButtonTrecker != '2'){
-            sortButton.src = 'img/Group34-greyUP.png';
-            sortButtonTrecker = '2'
-            showSortingIncreasing(textInputs);
+            sortButton.src = 'img/Group34-blackUP.png';
+            sortButtonTrecker = '2';
+            checkSortButton = '2';
+            showSortingIncreasing();
         } else if (sortButtonTrecker != '1') {
-            sortButton.src = 'img/Group34.svg';
-            showSortingDecreasing(textInputs);
-            sortButtonTrecker = '1'
+            sortButton.src = 'img/Group34-2.png';
+            showSortingDecreasing();
+            sortButtonTrecker = '1';
+            checkSortButton = '1';
         }
     });
+   
 }
 
 
 const button = document.querySelector('.button');
+
 const track = document.querySelector('.track');
-
 const tracker = document.querySelector('.tracker');
-const trackers = document.querySelectorAll('.tracker');
 
+const trackers = document.querySelectorAll('.tracker');
 const textInputs = document.querySelectorAll('.text-input');
+
 const buttonDelete = document.querySelector('.group56');
 
 const sortButton = document.querySelector('.group34')
 const sortButtonUP = document.querySelector('.group34-button')
 let dragElement;
+let checkSortButton = '1';
 
 button.addEventListener('click', addTrack);
 
@@ -118,12 +157,12 @@ buttonDelete.addEventListener('click', () => {
 
 trackers.forEach((item) => {
     item.addEventListener('dragstart', (evt) => {
-        evt.target.style.backgroundColor = '#FFDC40';
-        dragElement = evt.target;
+        item.style.backgroundColor = '#FFDC40';
+        dragElement = item;
     })
 
     item.addEventListener('dragend', (evt) => {
-        evt.target.style.backgroundColor = 'unset';
+        item.style.backgroundColor = 'unset';
         dragElement = null;
     })
 });
@@ -146,3 +185,28 @@ track.addEventListener('drop', (evt) => {
     evt.target.parentElement.style['border-bottom'] = '';
     track.insertBefore(dragElement, evt.target.parentElement);
 })
+
+
+
+
+
+sortButtonUP.addEventListener('mouseover', (event) => {
+    const sortButton = document.querySelector('.group34');
+
+    if (checkSortButton === '2'){
+        sortButton.src = 'img/Group34-blackUP.png';
+    } else if (checkSortButton === '1') {
+        sortButton.src = 'img/Group34-2.png';
+    }
+});
+
+sortButtonUP.addEventListener('mouseout', (event) => {
+    const sortButton = document.querySelector('.group34');
+
+    if (checkSortButton === '2'){
+        sortButton.src = 'img/Group34-greyUP.png';
+    } else if (checkSortButton === '1') {
+        sortButton.src = 'img/Group34.svg';
+    }
+});
+
